0487bc7a586a09daa44b5e2e926cc3bc680b8fa3
gulddaggi <81425764+gulddaggi@users.noreply.github.com>
2024-03-28 17:52:14 +0900

    Add Enemy character & Set tmp skeletalmesh
    적 캐릭터 스크립트 추가 및 임시 스켈레탈 메시 적용.
    추후 기획서 지정 메시로 변경 예정.

---
+++ Stiff/Source/Stiff/StiffEnemyCharacter.cpp
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "StiffEnemyCharacter.h"
+
+// Sets default values
+AStiffEnemyCharacter::AStiffEnemyCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	static ConstructorHelpers::FObjectFinder<USkeletalMesh> SK_MAN(TEXT("/Game/Resources/Man/Mesh/Full/SK_Man_Full_04.SK_Man_Full_04"));
+
+	if (SK_MAN.Succeeded())
+	{
+		GetMesh()->SetSkeletalMesh(SK_MAN.Object);
+	}
+
+	GetMesh()->SetRelativeLocationAndRotation(FVector(0.0f, 0.0f, -88.0f), FRotator(0.0f, -90.0f, 0.0f));
+
+}
+
+// Called when the game starts or when spawned
+void AStiffEnemyCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AStiffEnemyCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AStiffEnemyCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
